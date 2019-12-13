e8f562b2f592bba6924cb65e89a5ce3f2f4fd258
rubenrleyva <rubenrleyva@outlook.com>
2019-12-14 00:40:39 +0100

    Cambios_fotos

--- resources/views/layouts/comentarios.blade.php
+++ resources/views/layouts/comentarios.blade.php
@@ -1,31 +1,31 @@
 <!-- Area de comentarios
 ============================================ -->
-@if (isset($usuario->comentario))
+@if (isset($usuarios))
 <div id="testimonial-area" class="testimonial-area overlay overlay-white overlay-80 text-center pt-90 pb-90">
 	<div class="container">
 		<div class="row">
-			<div class="col-lg-8 col-12 mx-auto">
-				<!-- Slider fotos comentarios -->
+            <div class="col-lg-8 col-12 mx-auto">
+                <!-- Slider fotos comentarios -->
 				<div class="ti-slider mb-40">
-					@foreach($usuarios as $usuario)
-						@if (isset($usuario->comentario))
-							<div class="single-slide"><div class="image fix"><img src="{{ $usuario->foto->url_foto }}" alt="" /></div></div>
-						@endif
-					@endforeach
+                    @foreach($usuarios as $usuario)
+                        @if ($usuario->comentario)
+                            <div class="single-slide"><div class="image fix"><img src="{{ $usuario->foto->url_foto }}" alt="" /></div></div>
+                        @endif
+                    @endforeach
 				</div>
 				<!-- Slider comentarios -->
 				<div class="tc-slider">
-					@foreach($usuarios as $usuario)
-						@if (isset($usuario->comentario))
-							<div class="single-slide">
-							<p>{{ $usuario->comentario }}</p>
-							<h5>{{$usuario->name}}</h5>
-							<span>{{ $usuario->trabajo }}</span>
-						</div>
-						@endif
-					@endforeach
-				</div>
-			</div>
+                    @foreach($usuarios as $usuario)
+                        @if ($usuario->comentario)
+                            <div class="single-slide">
+                            <p>{{ $usuario->comentario }}</p>
+                            <h5>{{$usuario->name}}</h5>
+                            <span>{{ $usuario->trabajo }}</span>
+                        </div>
+                        @endif
+                    @endforeach
+                </div>
+            </div>
 		</div>
 	</div>
 	<!-- Slider -->
